beaef57eb764eda7f4b2193df95e9f44e3af0662
Khelifa Yassine <prostam27@gmail.com>
2020-12-25 22:10:40 +0100

    page home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class DefaultController extends Controller
 {
@@ -15,9 +18,12 @@ class DefaultController extends Controller
     {
          $em = $this->getDoctrine()->getManager();
          $categories = $em->getRepository("AdminBundle:Category")->findAll();
+
+       
         // replace this example code with whatever you need
         return $this->render('default/pages/home.html.twig', [
             'categories' => $categories,
+         
         ]);
     }
 
@@ -59,5 +65,55 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/admin.html.twig');
     }
+
+
+
+       /**
+     * @Route("/sma", name="smartphone_page")
+     */
+    public function pdAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/pages/store.html.twig');
+    }
+
+       /**
+     * @Route("/pgfrr", name="accessories_page")
+     */
+    public function pfdefrAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/pages/product.html.twig');
+    }
+
+       /**
+     * @Route("/prtr", name="repair_page")
+     */
+    public function prewtAction(Request $request)
+    {
+         
+         return $this->render('default/pages/test.html.twig');
+    }
+
+
+     /**
+     * @Route("/test", name="test_page")
+     * @Method( "POST")
+     */
+      public function ajaxAction(Request $request){
+        $arrayAjax = array("position" => "fasle");
+         if (($request->getMethod() == Request::METHOD_POST) && ($request->isXmlHttpRequest())) {
+            $personnage = $request->request->get('personnage');
+
+            $em = $this->getDoctrine()->getManager();
+            $categories = $em->getRepository("AdminBundle:Category")->findAll();
+
+            $arrayAjax = array(
+                "categories" => $categories[0]->getName(),
+            );
+         }
+        return new JsonResponse($arrayAjax);
+       }
+
 }
 
